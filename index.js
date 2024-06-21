acb0a3486e8404e32e87e27b120395317b338121
Jummanbd <85425626+Jummanbd@users.noreply.github.com>
2024-06-22 01:17:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ app.post("/api/register",upload.single("picture"), register );
  app.post('/api/post', upload.single("picture"),Post);
 app.use("/api", authRoutes);
 const PORT = 4000
-mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -59,14 +58,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
   .catch((error) => console.log(`${error} did not connect`));
-     mongoose.model('transcripts',
-
-    new mongoose.Schema({
-
-        Channel : String,
-
-        Content : Array
-
-    })
-
-)
